e1717bbc2fb256c46d83ddcde60254851e7f4480
norlana <pixikom@gmail.com>
2019-02-11 20:41:18 +0700

    Исправлен таймер обратного отсчета

--- functions.php
+++ functions.php
@@ -15,24 +15,16 @@ function include_template($name, $data) {
     $result = ob_get_clean();
 
     return $result;
-}
+};
 
 // Округляет число в большую сторону, форматирует и добавляет знак рубля
 function num_to_price($number) {
   
      return number_format(ceil($number),0,","," ") . ' ₽';   
-}
+};
 
 // Добавляет таймер обратного отсчет (интервал между 2-мя временными метками)
 function finish_time_counter() {
   
-    $check_time = strtotime('tomorrow') - time();
-
-    if ($check_time <= 0) {
-      
-        return '00:00';
-    } else {
-      
-      return date('s', floor($check_time / 3600)) . ':' . date('s', ceil(($check_time % 3600) / 60));
-    }
-}
+    return date_interval_format(date_diff(date_create('tomorrow'), date_create('now')), '%H:%I');
+};
